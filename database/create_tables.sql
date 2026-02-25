c7b3d41fc4d19baadf5f7bba3e6c914db75b24a3
Joseph Torsu <jostorsu@gmail.com>
2026-02-25 06:35:02 +0000

    Add database schema script for GoldBod

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -1,11 +1,17 @@
 CREATE DATABASE CommoditiesDB;
 USE CommoditiesDB;
 
-CREATE TABLE gold_prices_usd (
-    price_date DATE PRIMARY KEY,
-    usd_spot_oz FLOAT,
-    usd_goldbod_oz FLOAT,
-    usd_goldbod_lb FLOAT,
-    source NVARCHAR(50),
-    is_flagged BIT
-);
+
+CREATE TABLE [dbo].[gold_prices_usd](
+	[price_date] [date] NOT NULL,
+	[usd_spot_oz] [decimal](10, 2) NULL,
+	[usd_goldbod_oz] [decimal](10, 2) NULL,
+	[usd_goldbod_lb] [decimal](10, 2) NULL,
+	[source] [varchar](20) NULL,
+	[is_flagged] [bit] NULL,
+	[load_time] [datetime2](7) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[price_date] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
